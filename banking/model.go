f3dd942724a762c3573a6d6f513683ea2496271d
B2N10X30 <samuelbonux10@gmail.com>
2023-11-21 21:01:25 +0100

    banking

--- banking/model.go
+++ banking/model.go
@@ -16,7 +16,7 @@ type User struct {
 type Account struct {
 	User
 	IsRegistered  bool
-	Password      string
+	PIN           string
 	AccountNumber string
 	CreatedAt     time.Time
 	Balance       uint64
@@ -26,22 +26,53 @@ type Bank struct {
 	Customers []Account
 }
 
-func (a *Account) RegisterAccount(firstName, lastName, email, address, phoneNumber, password string, account uint64) (*Account, error) {
-	return &Account{
-			User: User{
-				FirstName:   firstName,
-				LastName:    lastName,
-				Email:       email,
-				Address:     address,
-				PhoneNumber: phoneNumber,
-			},
-			IsRegistered:  true,
-			Password:      HashPassword(password),
-			AccountNumber: GenerateAccountNum(),
-			CreatedAt:     time.Now(),
-			Balance:       000_000,
+func (a *Account) RegisterAccount(firstName, lastName, email, address, phoneNumber, pin string, account uint64) (*Account, error) {
+	const PINLenght = 4
+	if len(pin) < PINLenght || len(pin) > PINLenght {
+		return nil, fmt.Errorf("password should consist be 4 characters")
+	}
+	newUserAccount := Account{
+		User: User{
+			FirstName:   firstName,
+			LastName:    lastName,
+			Email:       email,
+			Address:     address,
+			PhoneNumber: phoneNumber,
 		},
-		nil
+		IsRegistered:  true,
+		PIN:           HashPassword(pin),
+		AccountNumber: GenerateAccountNum(),
+		CreatedAt:     time.Now(),
+		Balance:       000_000,
+	}
+	log.Printf("Account registered: AccountNumber=%s, FirstName=%s, LastName=%s, Email=%s",
+		a.AccountNumber, a.FirstName, a.LastName, a.Email)
+	return &newUserAccount, nil
+
+}
+
+func (a *Account) Deposit(amount uint64) (uint64, error) {
+	if amount <= 0 {
+		return a.Balance, fmt.Errorf("invalid deposit amount: %d", amount)
+	}
+	a.Balance += amount
+	return a.Balance, nil
+}
+
+func (a *Account) Withdraw(amount uint64) (uint64, error) {
+	if amount <= 0 || amount >= a.Balance {
+		return a.Balance, fmt.Errorf("invalid withdrawal amount: %d", amount)
+	}
+	a.Balance -= amount
+	return a.Balance, nil
+}
+
+func (a *Account) Transfer(amount uint64, accountNumber string) (uint64, error) {
+	if amount <= 0 || amount >= a.Balance {
+		return a.Balance, fmt.Errorf("invalid Transfer amount: %d", amount)
+	}
+	a.Balance -= amount
+	return a.Balance, nil
 }
 
 func HashPassword(password string) string {
@@ -49,7 +80,7 @@ func HashPassword(password string) string {
 	//cost :algo used to generate hash
 	hash, err := bcrypt.GenerateFromPassword(passwordbyte, bcrypt.DefaultCost)
 	if err != nil {
-		log.Fatalf("Error generating Hash:%v", err)
+		log.Printf("Error generating Hash:%v", err)
 	}
 	return string(hash)
 }
@@ -69,7 +100,30 @@ func GenerateAccountNum() string {
 
 }
 
-func (b *Bank) UpdateUserDetails(accountNumber, firstName, lastName, email, address, phoneNumber, pasword string) (*Account, error) {
+func GenerateOTP() (*string, error) {
+	//otp lenght
+	const otplenght = 6
+	//Use current time to create a new source everytime
+	source := rand.NewSource(time.Now().UnixNano())
+
+	//Random Number generator
+	randomGenerator := rand.New(source)
+	otp := ""
+	for i := 0; i < otplenght; i++ {
+		otp += fmt.Sprintf("%d", randomGenerator.Intn(10))
+	}
+
+	if len(otp) != otplenght {
+		return nil, fmt.Errorf("error generating otp: unexpected lenght")
+	}
+	return &otp, nil
+}
+
+func (b *Bank) UpdateUserDetails(accountNumber, firstName, lastName, email, address, phoneNumber, pin string) (*Account, error) {
+	const PINLenght = 4
+	if len(pin) < PINLenght || len(pin) > PINLenght {
+		return nil, fmt.Errorf("password should consist be 4 characters")
+	}
 	for i, customer := range b.Customers {
 		if customer.AccountNumber == accountNumber {
 			//all changeable details should be changed
@@ -78,10 +132,11 @@ func (b *Bank) UpdateUserDetails(accountNumber, firstName, lastName, email, addr
 			b.Customers[i].Email = email
 			b.Customers[i].Address = address
 			b.Customers[1].PhoneNumber = phoneNumber
-			b.Customers[i].Password = HashPassword(pasword)
+			b.Customers[i].PIN = HashPassword(pin)
 			//updated customer
 			return &b.Customers[i], nil
 		}
+		log.Printf("User details updated:\n FirstName: %s,\nLastName: %s,\nEmail: %s,\nAddress: %s,\nPhoneNumber: %s", firstName, lastName, email, address, phoneNumber)
 	}
 	return nil, fmt.Errorf("customer account for account number: %s not found", accountNumber)
 }
@@ -90,9 +145,11 @@ func (b *Bank) DeleteUserAccount(accountNumber string) (*[]Account, error) {
 	for i, customer := range b.Customers {
 		if customer.AccountNumber == accountNumber {
 			b.Customers = append(b.Customers[:i], b.Customers[i+1:]...)
+			log.Printf("Account deleted: %+v", customer)
 			return &b.Customers, nil
 		}
 	}
-	//if no account is not found
+	log.Printf("Account deleted: AccountNumber=%s", accountNumber)
+	// If no account is not found
 	return nil, fmt.Errorf("customer account for account number: %s not found", accountNumber)
 }
--- main.go
+++ main.go
@@ -1,11 +1,5 @@
 package main
 
-import (
-	"banker/banking"
-	"fmt"
-)
-
 func main() {
-	NewAccountNumber := banking.GenerateAccountNum()
-	fmt.Printf("New account is : %s", NewAccountNumber)
+
 }
